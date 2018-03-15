df78ea970743f71c888e1d96f1586aca605ab440
nidhogg5 <nidhogg5@yahoo.com.tw>
2018-03-16 00:18:43 +0800

    Revert "add html,js,smart contract"
    This reverts commit 483459d8c53104e22c512aa9ba91cc5c702269f4.

--- routes/index.js
+++ routes/index.js
@@ -3,24 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index.html');
+  res.render('index', { title: 'Express' });
 });
 
-//取得餘額
-router.get('/balance', function(req, res, next){
-  
-})
-
-//發送交易
-router.post('/transaction', function(req, res, next){
-  web3.eth.sendSignedTransaction(req.body.tx)
-  .on('receipt', function(result){
-    console.log(result)
-    res.send(result);
-  })
-  .on('error', , function(err){
-    console.log(error);
-    res.send("error")
-  })
-})
 module.exports = router;
